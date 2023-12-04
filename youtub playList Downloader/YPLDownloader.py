82e46946f437eb4b9b4cd6aab5886ebc380dabdd
younesben011 <younesbenzaama011@gmail.com>
2023-12-04 19:14:56 +0100

    nothing to mention

---
+++ youtub playList Downloader/YPLDownloader.py
@@ -0,0 +1,24 @@
+﻿from pytube import Playlist,YouTube
+links=[]
+# link = input("Enter YouTube Playlist url:")
+OUTPUT="F:\\tot\\" # downloading path
+
+
+
+
+for link in links:
+    yt_playlist = Playlist(link)
+
+    for video in yt_playlist.videos:
+        out_path=OUTPUT+yt_playlist.title
+        try:
+            video.streams.get_by_resolution(resolution="720p").download(output_path=out_path)
+            print(f" {video.title}: Done✔✔✔")
+        except :
+            try:
+                video.streams.get_lowest_resolution().download(output_path=out_path)
+                print(f" {video.title}: Done✔✔✔")
+            except:
+                print(f"video download field\n url: {video.title}")
+
+            
